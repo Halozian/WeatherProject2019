16684844dd6ef0809d76585d22e22434aabb440a
Halozian <Halozian@hotmail.com>
2019-09-27 15:20:44 +0200

    Weather Project 2019 final commit

--- src/main/java/Expleo/testCases/CompareWeather.java
+++ src/main/java/Expleo/testCases/CompareWeather.java
@@ -9,26 +9,26 @@ public class CompareWeather {
     public static void main(String[] args) {
         // Create a page object and retrieve the weather forecast for the next five days
         AccuweatherPageObject accuweatherForecast = AccuweatherForecast.getAccuweatherForecast();
-        System.out.println("Accuweather Forecast for Cape Town:");
+        System.out.println("Accuweather Forecast for Cape Town");
         accuweatherForecast.printBoth();
 
         // Create a page object and retrieve the weather forecast for the next five days
         WeatherNews24PageObject weatherNews24Forecast = WeatherNews24Forecast.getWeatherNews24Forecast();
-        System.out.println("Weather News 24 Forecast for Cape Town:");
+        System.out.println("Weather News 24 Forecast for Cape Town");
         weatherNews24Forecast.printBoth();
 
-        // Compare the max and min temperatures for both websites for the next five days
-        System.out.println("\nWeather Site Comparisons for Cape Town");
+        // Compare the max and min temperatures for both websites
+        System.out.println("\n\n\nWeather Site Comparisons for Cape Town");
         for (int i = 0; i < 5; i++) {
-            System.out.println("\nCape Town Weather Day " + (i+1) + " Comparison");
-            System.out.print("AW Max: " + accuweatherForecast.getMaxTemp(i) + "\t\tWN24 Max: " + weatherNews24Forecast.getMaxTemp(i));
+            System.out.println("\nDay " + (i+1) + " Comparison");
+            System.out.print("AW Max: " + accuweatherForecast.getMaxTemp(i) + " °C\t\tWN24 Max: " + weatherNews24Forecast.getMaxTemp(i) + " °C");
             if (accuweatherForecast.getMaxTemp(i) == weatherNews24Forecast.getMaxTemp(i))
                 System.out.println("\t\t[Max Temperatures are equal]");
             else
                 System.out.println("\t\t[Max Temperatures are different]");
-            System.out.print("AW Min: " + accuweatherForecast.getMinTemp(i) + "\t\tWN24 Min: " + weatherNews24Forecast.getMinTemp(i));
+            System.out.print("AW Min: " + accuweatherForecast.getMinTemp(i) + " °C\t\tWN24 Min: " + weatherNews24Forecast.getMinTemp(i) + " °C");
             if (accuweatherForecast.getMinTemp(i) == weatherNews24Forecast.getMinTemp(i))
-                System.out.println("\t\t[Min Temperatures are equal");
+                System.out.println("\t\t[Min Temperatures are equal]");
             else
                 System.out.println("\t\t[Min Temperatures are different]");
         }
